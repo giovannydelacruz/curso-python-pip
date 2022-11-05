28df0b8dc80d7b94d319164541e8844bfb4d2981
giovannydelacruz <giovannydelacruz10@gmail.com>
2022-11-05 15:19:35 -0500

    Terminando el curso de python y entornos virtuales

--- app/charts.py
+++ app/charts.py
@@ -10,7 +10,7 @@ def generate_pie_chart(labels,values):
     fig, ax = plt.subplots()
     ax.pie(values,labels=labels)
     ax.axis("equal")
-    plt.savefig("pie.png")
+    plt.savefig("chart_pie_final_este_si.png")
     plt.close()
 
 if __name__ == "__main__":
@@ -18,9 +18,3 @@ if __name__ == "__main__":
     values = [10,40,800]
     generate_bar_chart(labels,values)
     generate_pie_chart(labels,values)
-
-
-
-
-
-
